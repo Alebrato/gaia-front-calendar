8e1f79f4e9c45b86eb5fa9ee80b642094386b196
andresordport <andres_herrero@hotmail.es>
2019-05-28 16:26:19 +0200

    Tenemos los dos ejemplos instalados, falta mezclarl las funcionalidades de ambos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -53,6 +53,8 @@ import { DemoComponent } from './components/administracion/demo/demo.component';
 import { CommonModule } from '@angular/common';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
@@ -118,7 +120,9 @@ import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
     CalendarModule.forRoot({
       provide: DateAdapter,
       useFactory: adapterFactory
-    })
+    }),
+    FlatpickrModule.forRoot(),
+    NgbModalModule
   ],
   providers: [
     {
--- src/app/components/administracion/demo/demo.component.ts
+++ src/app/components/administracion/demo/demo.component.ts
@@ -1,7 +1,7 @@
-import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit, ViewChild, TemplateRef } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-import { CalendarEvent } from 'angular-calendar';
+import { CalendarEvent, CalendarEventAction, CalendarEventTimesChangedEvent, CalendarView } from 'angular-calendar';
 import {
   isSameMonth,
   isSameDay,
@@ -11,10 +11,18 @@ import {
   endOfWeek,
   startOfDay,
   endOfDay,
-  format
+  format,
+  subDays,
+  addDays,
+  addHours
 } from 'date-fns';
 import { Observable } from 'rxjs';
+import { Subject } from 'rxjs';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { colors } from '../../../../demo-utils/colors';
+import {
+
+} from 'angular-calendar';
 
 interface Film {
   id: number;
@@ -38,74 +46,164 @@ function getTimezoneOffsetString(date: Date): string {
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './demo.component.html',
 })
-export class DemoComponent implements OnInit {
-  view: string = 'month';
+// export class DemoComponent implements OnInit {
+export class DemoComponent {
+  @ViewChild('modalContent') modalContent: TemplateRef<any>;
+
+  view: CalendarView = CalendarView.Month;
+  CalendarView = CalendarView;
 
   viewDate: Date = new Date();
 
-  events$: Observable<Array<CalendarEvent<{ film: Film }>>>;
+  modalData: {
+    action: string;
+    event: CalendarEvent;
+  };
 
-  activeDayIsOpen: boolean = false;
+  actions: CalendarEventAction[] = [
+    {
+      label: '<i class="fa fa-fw fa-pencil"></i>',
+      onClick: ({ event }: { event: CalendarEvent }): void => {
+        this.handleEvent('Edited', event);
+      }
+    },
+    {
+      label: '<i class="fa fa-fw fa-times"></i>',
+      onClick: ({ event }: { event: CalendarEvent }): void => {
+        this.events = this.events.filter(iEvent => iEvent !== event);
+        this.handleEvent('Deleted', event);
+      }
+    }
+  ];
+  refresh: Subject<any> = new Subject();
 
-  constructor(private http: HttpClient) {}
+  // events$: Observable<Array<CalendarEvent<{ film: Film }>>>;
+  events: CalendarEvent[] = [
+    {
+      start: subDays(startOfDay(new Date()), 1),
+      end: addDays(new Date(), 1),
+      title: 'A 3 day event',
+      color: colors.red,
+      actions: this.actions,
+      allDay: true,
+      resizable: {
+        beforeStart: true,
+        afterEnd: true
+      },
+      draggable: true
+    },
+    {
+      start: startOfDay(new Date()),
+      title: 'An event with no end date',
+      color: colors.yellow,
+      actions: this.actions
+    },
+    {
+      start: subDays(endOfMonth(new Date()), 3),
+      end: addDays(endOfMonth(new Date()), 3),
+      title: 'A long event that spans 2 months',
+      color: colors.blue,
+      allDay: true
+    },
+    {
+      start: addHours(startOfDay(new Date()), 2),
+      end: new Date(),
+      title: 'A draggable and resizable event',
+      color: colors.yellow,
+      actions: this.actions,
+      resizable: {
+        beforeStart: true,
+        afterEnd: true
+      },
+      draggable: true
+    }
+  ];
 
-  ngOnInit(): void {
-    this.fetchEvents();
-  }
+  // activeDayIsOpen: boolean = false;
+  activeDayIsOpen = true;
 
-  fetchEvents(): void {
-    const getStart: any = {
-      month: startOfMonth,
-      week: startOfWeek,
-      day: startOfDay
-    }[this.view];
 
-    const getEnd: any = {
-      month: endOfMonth,
-      week: endOfWeek,
-      day: endOfDay
-    }[this.view];
+  constructor(private modal: NgbModal/*, private http: HttpClient*/) {}
 
-    const params = new HttpParams()
-      .set(
-        'primary_release_date.gte',
-        format(getStart(this.viewDate), 'YYYY-MM-DD')
-      )
-      .set(
-        'primary_release_date.lte',
-        format(getEnd(this.viewDate), 'YYYY-MM-DD')
-      )
-      .set('api_key', '0ec33936a68018857d727958dca1424f');
+  // ngOnInit(): void {
+  //   this.fetchEvents();
+  // }
 
-    this.events$ = this.http
-      .get('https://api.themoviedb.org/3/discover/movie', { params })
-      .pipe(
-        map(({ results }: { results: Film[] }) => {
-          return results.map((film: Film) => {
-            return {
-              title: film.title,
-              start: new Date(
-                film.release_date + getTimezoneOffsetString(this.viewDate)
-              ),
-              color: colors.yellow,
-              allDay: true,
-              meta: {
-                film
-              }
-            };
-          });
-        })
-      );
-  }
+  // fetchEvents(): void {
+  //   const getStart: any = {
+  //     month: startOfMonth,
+  //     week: startOfWeek,
+  //     day: startOfDay
+  //   }[this.view];
 
-  dayClicked({
-    date,
-    events
-  }: {
-    date: Date;
-    events: Array<CalendarEvent<{ film: Film }>>;
-  }): void {
+  //   const getEnd: any = {
+  //     month: endOfMonth,
+  //     week: endOfWeek,
+  //     day: endOfDay
+  //   }[this.view];
+
+  //   const params = new HttpParams()
+  //     .set(
+  //       'primary_release_date.gte',
+  //       format(getStart(this.viewDate), 'YYYY-MM-DD')
+  //     )
+  //     .set(
+  //       'primary_release_date.lte',
+  //       format(getEnd(this.viewDate), 'YYYY-MM-DD')
+  //     )
+  //     .set('api_key', '0ec33936a68018857d727958dca1424f');
+
+  //   this.events$ = this.http
+  //     .get('https://api.themoviedb.org/3/discover/movie', { params })
+  //     .pipe(
+  //       map(({ results }: { results: Film[] }) => {
+  //         return results.map((film: Film) => {
+  //           return {
+  //             title: film.title,
+  //             start: new Date(
+  //               film.release_date + getTimezoneOffsetString(this.viewDate)
+  //             ),
+  //             color: colors.yellow,
+  //             allDay: true,
+  //             meta: {
+  //               film
+  //             }
+  //           };
+  //         });
+  //       })
+  //     );
+  // }
+
+  // dayClicked({
+  //   date,
+  //   events
+  // }: {
+  //   date: Date;
+  //   events: Array<CalendarEvent<{ film: Film }>>;
+  // }): void {
+  //   if (isSameMonth(date, this.viewDate)) {
+  //     if (
+  //       (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+  //       events.length === 0
+  //     ) {
+  //       this.activeDayIsOpen = false;
+  //     } else {
+  //       this.activeDayIsOpen = true;
+  //       this.viewDate = date;
+  //     }
+  //   }
+  // }
+
+  // eventClicked(event: CalendarEvent<{ film: Film }>): void {
+  //   window.open(
+  //     `https://www.themoviedb.org/movie/${event.meta.film.id}`,
+  //     '_blank'
+  //   );
+  // }
+
+  dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
     if (isSameMonth(date, this.viewDate)) {
+      this.viewDate = date;
       if (
         (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
         events.length === 0
@@ -113,15 +211,59 @@ export class DemoComponent implements OnInit {
         this.activeDayIsOpen = false;
       } else {
         this.activeDayIsOpen = true;
-        this.viewDate = date;
       }
     }
   }
 
-  eventClicked(event: CalendarEvent<{ film: Film }>): void {
-    window.open(
-      `https://www.themoviedb.org/movie/${event.meta.film.id}`,
-      '_blank'
-    );
+  eventTimesChanged({
+    event,
+    newStart,
+    newEnd
+  }: CalendarEventTimesChangedEvent): void {
+    this.events = this.events.map(iEvent => {
+      if (iEvent === event) {
+        return {
+          ...event,
+          start: newStart,
+          end: newEnd
+        };
+      }
+      return iEvent;
+    });
+    this.handleEvent('Dropped or resized', event);
+  }
+
+  handleEvent(action: string, event: CalendarEvent): void {
+    this.modalData = { event, action };
+    this.modal.open(this.modalContent, { size: 'lg' });
+  }
+
+  addEvent(): void {
+    this.events = [
+      ...this.events,
+      {
+        title: 'New event',
+        start: startOfDay(new Date()),
+        end: endOfDay(new Date()),
+        color: colors.red,
+        draggable: true,
+        resizable: {
+          beforeStart: true,
+          afterEnd: true
+        }
+      }
+    ];
+  }
+
+  deleteEvent(eventToDelete: CalendarEvent) {
+    this.events = this.events.filter(event => event !== eventToDelete);
+  }
+
+  setView(view: CalendarView) {
+    this.view = view;
+  }
+
+  closeOpenMonthViewDay() {
+    this.activeDayIsOpen = false;
   }
 }
